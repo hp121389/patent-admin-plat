c6e9ac83517d661a4ccec9f5c7eeae02891101b3
86186 <1361601920@qq.com>
2022-10-22 14:05:06 +0800

    package insert&delete

--- app/user-agent/models/package.go
+++ app/user-agent/models/package.go
@@ -11,6 +11,15 @@ type Package struct {
 	models.ModelTime
 }
 
+type UserPackage struct {
+	models.Model
+	PackageId int `gorm:"foreignKey:PackageId;comment:PackageId" json:"PackageId" `
+	UserId    int `gorm:"comment:用户ID"  json:"UserId"`
+	ID        int `gorm:"primaryKey;autoIncrement;comment:编码" json:"Id" `
+	models.ControlBy
+	models.ModelTime
+}
+
 func (e *Package) TableName() string {
 	return "package"
 }
@@ -22,4 +31,16 @@ func (e *Package) Generate() models.ActiveRecord {
 
 func (e *Package) GetId() interface{} {
 	return e.PackageId
+}
+
+func (e *UserPackage) GetPackageId() interface{} {
+	return e.PackageId
+}
+
+func (e *UserPackage) TableName() string {
+	return "user_package"
+}
+
+func (e *UserPackage) GetUserId() interface{} {
+	return e.UserId
 }

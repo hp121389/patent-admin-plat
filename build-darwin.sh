0b8edcdcdb899f7c7638cc15b5c04a7a9b68eca3
86186 <1361601920@qq.com>
2022-12-23 22:15:02 +0800

    commit

--- build-darwin.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-CGO_ENABLED=0 go build -o bin/PatentAdminPlat-darwin .
--- cmd/api/patent.go
+++
@@ -1,7 +0,0 @@
-package api
-
-import "go-admin/app/user-agent/router"
-
-func init() {
-	AppRouters = append(AppRouters, router.InitRouter)
-}
